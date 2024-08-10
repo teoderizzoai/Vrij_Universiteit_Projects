c53f7603f01b8e2797845c39ac348f5031398667
teoderizzoai <teo.derizzo.ai@gmail.com>
2024-08-10 14:28:17 +0200

    Delete Programming in Python for Text Analysis directory

--- Programming in Python for Text Analysis/Assignment_2/assignment2_utils.py
+++
@@ -1,6 +0,0 @@
-#function for assignment 2, exercise 6d
-def clean_text_general(text, chars_to_remove={'\n', ',', '.', '"'}):
-    new_text = text
-    for char in chars_to_remove:
-        new_text = new_text.replace(char, " ")
-    return str(new_text)
